f6ed8a7fb67be6c4cc2df2100bca80c1ebf67abe
Miguel Angel <miguel.angel.lopez.vicente@gmail.com>
2014-04-19 23:04:17 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,10 @@
+#SteamAutoGift 
+Esta pequeña aplicación controla Firefox para apuntarse a los distintos sorteos de las llaves de Steam.
+
+Para ello, lee el fichero _favorities.txt_ y busca todos los sorteos concordantes. 
+
+Finalmente, se apunta al resto de sorteos de la primera página.
+
+-----------
+#Uso
+Para usarlo debéis tener Firefox instalado y correctamente identificado en SteamGift
---
+++ src/main/java/com/malv/steamgift/SteamGifts.java
@@ -0,0 +1,94 @@
+package com.malv.steamgift;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+
+public class SteamGifts {
+	
+	
+	
+	private FirefoxDriver firefox;
+	private ArrayList<String> favorites;
+	
+	
+	private static final String URL = "http://www.steamgifts.com";
+	private static final String URL_SEARCH = "http://www.steamgifts.com/open/search/";
+	
+	public SteamGifts() {
+		ProfilesIni profilesIni = new ProfilesIni();
+		FirefoxProfile profile = profilesIni.getProfile("default");
+		firefox = new FirefoxDriver(profile);
+		favorites = new ArrayList<String>();
+	}
+	
+	
+	public void loadFavorites() {
+		try {
+			
+			BufferedReader file = new BufferedReader(new FileReader("favorities.txt"));
+			for (String line = file.readLine(); line != null; line = file.readLine())
+				favorites.add(line);
+			
+				
+			file.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+				
+	}
+	
+	
+	public void open(String page) {
+		
+		firefox.get(page);
+		List<WebElement> enlaces = firefox.findElements(By.cssSelector(".post .title a"));
+		
+		ArrayList<String> links = new ArrayList<String>(enlaces.size());
+		
+		for (WebElement a : enlaces)
+			links.add(a.getAttribute("href"));
+		
+		
+		for (String a : links) {
+			
+			firefox.get(a);
+			
+			if (firefox.findElementsByCssSelector(".submit_entry").size() > 0)
+				firefox.findElementByCssSelector(".submit_entry").click();
+					
+		
+		}
+		
+		
+	}
+	
+
+	public void start() {
+		for (String s : favorites)
+			open(URL_SEARCH + s.replace(" ", "%20"));
+		
+		open(URL);
+		
+		firefox.close();
+	}
+	
+	
+	public static void main (String argv[]) {
+		SteamGifts steamgifts = new SteamGifts();
+		steamgifts.loadFavorites();
+		steamgifts.start();
+		
+	}
+	
+	
+	
+}
